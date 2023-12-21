ccf5cfb46c540a2b2a4f8678aaf4683b8441a98e
Trinadh Rayala <3nadhmail@gmail.com>
2023-12-21 12:55:18 -0600

    Components

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,17 +1,133 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from  "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+  
+
+function App(){
+    return <div className = "container">
+        <h1>Hello React! Trinadh Rayala</h1>
+        <p>Trinadh Rayala</p>
+        <Header />
+        <Menu />
+
+        <Footer />
+        
+    </div>
+}
+
+//creating the Pizza Component
+
+function Pizza(){
+    return(
+    <div>
+   
+    <img src = "pizzas/focaccia.jpg" alt = "pizza Foccacia"/>
+    <div>
+
+    <strong>ingredients:</strong>
+    <p>"Bread with italian olive oil and rosemary"</p>
+    </div>
+    </div>
+    );
+}
+
+function Header(){
+    const style = {
+        color:"red", 
+        fontSize : "28px",
+        textTransform : "uppercase"
+    };
+    return(
+        <header className = "header">
+        <h1 style = {style} >
+        Trinadh Fast React Pizza Co.
+        </h1>
+
+        </header>
+    );
+}
+
+function Menu(){
+   return(
+    <main className = "menu">
+        <div>
+            <Pizza />
+            <Pizza />
+        </div>
+    </main>
+   );
+}
+
+function Footer(){
+
+    const hour = new Date().getHours();
+    const openHour = 19;
+    const closedHour = 20;
+
+    const isOpen = hour >= openHour && hour <= closedHour;
+    console.log(isOpen);
+    return(
+        <footer className = "footer">
+        <div>
+        <footer>{new Date().toLocaleTimeString()}.we're currently open.</footer>
+        </div>
+        </footer>
+    );
+}
+
+
+//React 18
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//React before 18
+//React.render(<App />, document.getElementById("root"));
