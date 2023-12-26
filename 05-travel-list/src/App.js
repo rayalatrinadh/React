2384daca8d8f13435df86469c02d8e5905575433
Trinadh Rayala <3nadhmail@gmail.com>
2023-12-26 15:05:51 -0600

    beforeEachComponentTravelList

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -7,25 +7,47 @@ const initialItems = [
 ];
 
 export default function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(initialItems);
 
-  function handleAddItems(item ) {
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+  function handleAddItems(item) {
     // in react everything is immutalbe
     //  here is the syntax to updae the list object
     // ...items -> it means the previous list is fetching
     // and then add new item to the list
-    console.log(" Before : item in App() : " +  item);
     //setItems((items) => [...items, {initialItems}]);
     setItems((items) => [...items, item]);
-    console.log(" After : item in App() : " +  item);
   }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList(){
+    const confirmed = window.confirm("Are you sure you want to delete all items?");
+
+    if(confirmed)
+    setItems([]);
+  }
+
   return (
     <div className="app">
       <Logo />
       {/* in this form component passing function */}
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
-      <Stats />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList = {handleClearList}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -45,7 +67,6 @@ function Form({ onAddItems }) {
 
     //configuring the new Item
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
 
     //form prop
     onAddItems(newItem);
@@ -82,37 +103,89 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
-console.log("items : " +  {items});
+function PackingList({ items, onDeleteItem, onToggleItem, onClearList}) {
+  const [sortBy, setSortBy] = useState("input");
   
+  let sortedItems;
+
+  if(sortBy === "input") sortedItems = items;
+
+  if(sortBy === "description")
+    sortedItems = items.slice().sort((a,b) => a.description.localeCompare(b.description));
+
+  if(sortBy === "packed")
+    sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
+
+    
   return (
     <div className="list">
       {/* <ul> */}
       <li>
-        {items.map((item) => (
-          <Item item={item} key={item.id} />
+        {sortedItems.map((item) => (
+          // Actually onDeleteItem is getting from the App() and now from here the onDeleteItem is passed to
+          // the Item component
+          // NOTE : REMEMBER THIS POINT . whenever the button is clicked the id is passed BACK from the
+          // Item to here(PackingList) and from here the id goes to the App()
+          // and then after the list is updated as per the logic writter in the App() -> handleDeleteItem
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
         ))}
       </li>
       {/* </ul> */}
+
+      <div className = "actions">
+        <select value = {sortBy} onChange={(e)=>setSortBy(e.target.value)}> 
+          <option value = "input">Sort by input order</option>
+          <option value = "description"> Sort by Description</option>
+          <option value = "packed"> Sort by packed status</option>
+        </select>
+        <button onClick = {onClearList}>CLEAR LIST</button>
+      </div>
+      
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      {/* function to pass the argument(id) to the packingList from the Item  */}
+      <button onClick={() => onDeleteItem(item.id)}> ❌</button>
     </li>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="footer">
+        <em>Start adding some items to your packing list 🛩️</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>👜 you have x items on your list, and you already packed x (x%).</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : `👜 you have ${numItems} items on your list, and you already ${numPacked}  (${percentage}%)`}
+        .
+      </em>
     </footer>
   );
 }
