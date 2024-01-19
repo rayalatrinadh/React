ce9290065603d39d429b85da06b3d79f6e7972d8
Trinadh Rayala <3nadhmail@gmail.com>
2024-01-19 05:45:12 -0600

    apiLoaderStatesofQuiz

---
+++ quiz-app/src/StartScreen.js
@@ -0,0 +1,12 @@
+function StartScreen({numQuestions}) {
+    return (
+        <div className = "start">
+            <h2> Welcome to The React Quiz!</h2>
+                <h3> {numQuestions} question to test your React mastery</h3>
+                <button>Let's start</button>
+            
+        </div>
+    )
+}
+
+export default StartScreen
