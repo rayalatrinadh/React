8b5046d67a4c666fae075d3a709979f184de44e6
Trinadh Rayala <3nadhmail@gmail.com>
2024-01-18 18:25:31 -0600

    quizUpdates

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,12 +1,18 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
 
 export default function App(){
   return(
-    <div>
-      <p>
-        Trinadh Rayala
-        <DateCounter />
-      </p>
+    <div className = "app">
+      
+        <Header />
+
+        <Main>
+          <p>1/15</p>
+          <p>Question?</p>
+        </Main>
+
+        
     </div>
   );
 }
