e40881e51b0af8e91e6311d84a3c5225b51ddc25
Trinadh Rayala <3nadhmail@gmail.com>
2024-01-31 16:42:52 -0600

    Vit+React_Route

---
+++ 11-worldwise/src/pages/Pricing.jsx
@@ -0,0 +1,11 @@
+import PageNav from "../components/PageNav";
+function Pricing() {
+    return (
+        <div>
+            <PageNav />
+            pricing
+        </div>
+    )
+}
+
+export default Pricing
