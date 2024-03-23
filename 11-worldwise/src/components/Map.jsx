175c428331e0030724cdd2a67482ac2f573c5526
Trinadh Rayala <3nadhmail@gmail.com>
2024-03-23 12:06:03 -0500

    InteractingWithTheMap

--- 11-worldwise/src/components/Map.jsx
+++ 11-worldwise/src/components/Map.jsx
@@ -1,38 +1,47 @@
-import styles from './Map.module.css'
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import styles from "./Map.module.css";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+import {useState} from "react";
+import {useCities} from "../contexts/CitiesContext";
+
+
+
 function Map() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const {cities} = useCities();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer} 
+    >
+      <MapContainer 
+      center={mapPosition} 
+      zoom={13} 
+      scrollWheelZoom={true}
+      className = {styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+       {cities.map((city) => 
+        <Marker position={[city.position.lat, city.position.lng]} 
+        key = {city.id}>
+        <Popup>
+         <span>{city.emoji} {city.country}</span>
+        </Popup>
+      </Marker>)}
 
 
 
-    const [searchParams, setSearchParams] = useSearchParams();
-
-    const lat = searchParams.get("lat");
-    const lng = searchParams.get("lng");
-
-
-    return (
-        <div className = {styles.mapContainer} 
-        onClick = {()=>navigate("form")}>
-            <h1>Map</h1>
-            <h1>
-                Position : 
-                lat : {lat}
-                lng : {lng}
-               
-            </h1>
-
-            <button
-            onClick = {() =>{
-                setSearchParams({lat : 21, lng:59})
-            }}
-            >
-                Change Pos
-            </button>
-
-        </div>
-    )
+      </MapContainer>
+    </div>
+  );
 }
 
-
-export default Map
+export default Map;
